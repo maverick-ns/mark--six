f125136a3f145b90e18f8aaa9e8a503af27a5bd3
maverickJAI <jaimech61@gmail.com>
2021-08-06 07:48:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var btntranslate = document.querySelector("#btn-translate");
-var textarea = document.querySelector("#textarea");
+var textarea = document.querySelector("#txtarea");
+var divoutput = document.querySelector( "#output");
 
-console.log(textarea)
-
+console.log.div( "cbsjbdshjbdshj"+textarea.value);
 
 function clickEventhandler(){
     console.log("clicked"),
     console.log("input",textarea.value);
-
+   
  } 
 
 btntranslate.addEventListener("click", clickEventhandler);
